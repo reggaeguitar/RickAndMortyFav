c7ffff3023e3f7b598fa625c0386effde27f528b
Joe Brady <joseph.b.brady@gmail.com>
2020-02-28 10:22:35 -0700

    Adding IContext interface

--- src/Store.tsx
+++ src/Store.tsx
@@ -6,7 +6,17 @@ const initialState: IState = {
   favorites: []
 };
 
-export const Store = React.createContext<IState | any>(initialState);
+const initialContext: IContext = {
+  state: initialState,
+  dispatch: () => {}
+}
+
+interface IContext {
+  state: IState
+  dispatch: React.Dispatch<IAction>
+}
+
+export const Store = React.createContext<IContext>(initialContext);
 
 function reducer(state: IState, action: IAction): IState {
   switch (action.type) {
